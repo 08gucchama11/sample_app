17d7d981c1a6edf097c735f2d9a6e5ccc42014ec
08gucchama11 <296365775+08gucchama11@users.noreply.github.com>
2026-07-08 02:37:07 +0000

    [Finish] 3-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,9 @@
 Rails.application.routes.draw do
-  get "lists/new"
-  post 'lists' => 'lists#create'   #ここを追記します
+  get 'lists/new'
+  get 'top' => 'home#top'
+  post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
-  # .../lists/1 や .../lists/3 に該当する
   get 'lists/:id' => 'lists#show', as: 'list'
-  get "lists/index"
-  # .../lists/1 や .../lists/3 に該当する
-  get 'lists/:id' => 'lists#show'
-  get "lists/edit"
-  get '/top' => 'homes#top'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
 end
